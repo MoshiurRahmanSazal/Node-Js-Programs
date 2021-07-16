650e23f1f572064f2b057ef434439d3a3eaa1a8c
Moshiur Rahman Sazal <80117611+MoshiurRahmanSazal@users.noreply.github.com>
2021-07-16 22:37:18 +0600

    Add files via upload

---
+++ Basic/LocalModule.js
@@ -0,0 +1,7 @@
+const Lm = require ('./localExp');
+
+console.log(Lm.getMgs());
+console.log(Lm.getName());
+console.log(Lm.cgpa);
+
+
---
+++ Basic/localExp.js
@@ -0,0 +1,21 @@
+const getMgs = () => {
+    return "Whatss up";
+}
+const getName = () =>{
+    return "Moshiur Rahman Sazal";
+}
+const getMail = () => {
+    return "Saxal.Iubat@gmail.com";
+}
+
+const cgpa =3.70;
+
+
+
+
+module.exports ={
+    getMgs,
+    getMail,
+    getName,
+    cgpa
+}
